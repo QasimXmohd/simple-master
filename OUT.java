86eb2fa1b896587790a5b40fee8f9d31356ba867
Qasim mohd <qassimkhalifa8@gmail.com>
2022-05-23 09:49:44 +0300

    NEWX

--- OUT.java
+++ OUT.java
@@ -10,7 +10,7 @@ public class OUT {
 			double z = x + y;
 
 System.out.print("hello world this is Qasim \n jenkins github connection fine "+
-"\n we are in year 2022 \n adition of x and y is " + z);
+"\n we are in year 2022 \n adition of x and y IS " + z);
 
 System.out.println("\n pi time r^2 = "+Math.PI*Math.sqrt(z));
 System.out.println("\n HEY I HOP 403 IS GONE \n 403 gone \n atm 403 ");
